b8a645e212a802813cfb6429e39b4d29793fdc0e
Leonardo Rafael de Castro <lrccastro4@gmail.com>
2026-07-24 21:14:19 -0300

    Algumas coisas a mais

--- variantes/structured/structured.py
+++ variantes/structured/structured.py
@@ -7,10 +7,22 @@ dtype_proprio_com_dict = np.dtype({'names': ['nome', 'idade', 'departamento', 's
 
 # Quando um array for criado ele pode ter seu valores acessados via index, chave ou os 2 juntos
 funcionario1 = ('Leonardo', 18, 'analise de dados', 1)
-funcioario2 = ('Rafael', 29, 'engenharia', 0)
-lista_de_funcionarios = [funcionario1, funcioario2]
+funcioario2 = ('Rafael', 29, 'RH', 0)
+funcioario3 = ('Bianca', 31, 'marketing', 1)
+funcioario4 = ('Pedro', 25, 'engenharia', 1)
+funcioario5 = ('Kaua', 25, 'marketing', 0)
+funcioario6 = ('Fernando', 21, 'analise de dados', 1)
+lista_de_funcionarios = [funcionario1, funcioario2, funcioario3, funcioario4, funcioario5, funcioario6]
 
 funcionarios = np.array(lista_de_funcionarios,  dtype=dtype_proprio)
 print(funcionarios["nome"])
 print(funcionarios[0])
 print(funcionarios[1]["idade"])
+
+# E possivel ver os campos da estrutura do array
+print(funcionarios.dtype)
+print("\n")
+
+# E possivel ordenar os itens do array com o attr order
+for i in np.sort(funcionarios, order=['status', 'nome']):
+    print(i)
