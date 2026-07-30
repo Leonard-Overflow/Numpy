092bc463ad908a86388ec1d815af8ba76382af4c
Leonardo Rafael de Castro <lrccastro4@gmail.com>
2026-07-30 20:17:54 -0300

    Arrumando alguns detalhes

--- numeros_aleatorios/random_com_numpy.py
+++ numeros_aleatorios/random_com_numpy.py
@@ -1,5 +1,5 @@
 from numpy import random
 
 print(random.rand(3, 3)) # O a 1 por padrao
-print(random.randn(3, 3)) # Dstribuicao normal
+print(random.randn(3, 3)) # Distribuicao normal com o centro sendo 0
 print(random.randint(0, 10, 4)) # 4 inteiros aleatorios de o a 10
