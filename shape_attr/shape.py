4ec89a9a46e2a8d2d9c8b38d63d2112b3ea00579
Leonardo Rafael de Castro <lrccastro4@gmail.com>
2026-07-31 21:43:16 -0300

    Shape

---
+++ shape_attr/shape.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+# Shapes iguais são compativeis e as operacoes se aplicam elemento a elemento
+
+# Shapes diferentes, mas compativeis
+vetor1 = np.array([1, 2, 3])
+vetor2 = np.array([[10],
+                   [20]])
+
+print(vetor1.shape)
+print(vetor2.shape)
+print(vetor1 + vetor2)
+
+# As qtds de linhas e colunas do primeiro e do segundo devem ser iguais.
+# O resultado e uma tabela formada com os valores de cada um dos arrays
+# Quando linhas e colunas nao coinscidem os arrays nao sao compativeis.
